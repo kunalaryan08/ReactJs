90328f6b632238e74adf6dea20b10242bb6710ce
kunalaryan08 <kunalaryan04@gmail.com>
2025-04-04 00:17:46 +0530

    4apr

---
+++ lec12/React-Router/src/components/Github/Github.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState} from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    const data = useLoaderData();
+    // const [data, setData] = useState([]);
+    // useEffect(()=>{
+    //     fetch('https://api.github.com/users/hiteshchoudhary')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data);
+    //         setData(data);
+    //     })
+    // }, [])
+  return (
+    <div className='text-gray-300 bg-gray-900 p-8 text-center'>Github : {data.followers}
+    <img src={data.avatar_url} alt="Github Image" width={400} />
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+    return response.json();
+}
